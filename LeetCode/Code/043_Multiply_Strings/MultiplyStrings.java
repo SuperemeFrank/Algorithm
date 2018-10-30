ede156ddbc44b870606fa01239eb8837d01f3641
SuperemeFrank <frank_haonanyu@163.com>
2018-10-30 17:16:51 -0400

    s

--- LeetCode/Code/043_Multiply_Strings/MultiplyStrings.java
+++ LeetCode/Code/043_Multiply_Strings/MultiplyStrings.java
@@ -1,5 +1,41 @@
 class Solution {
     public String multiply(String num1, String num2) {
+    int m = num1.length(), n = num2.length();
+    int[] pos = new int[m + n];
+   
+    for(int i = m - 1; i >= 0; i--) {
+        for(int j = n - 1; j >= 0; j--) {
+            int mul = (num1.charAt(i) - '0') * (num2.charAt(j) - '0'); 
+            int p1 = i + j, p2 = i + j + 1;
+            int sum = mul + pos[p2];
+
+            pos[p1] += sum / 10; // we don't need to consider if p1 > 10 here, cause it will be handled afterward
+            pos[p2] = (sum) % 10;
+        }
+    }  
+    
+    StringBuilder sb = new StringBuilder();
+    for(int p : pos) if(!(sb.length() == 0 && p == 0)) sb.append(p);
+    return sb.length() == 0 ? "0" : sb.toString();
+    }
+}
+
+/* Time: O(n ^ 2)  Space: O(n + m)
+
+****the ith digit in num1 product the jth digit in num2 affect (i + j)th and (i + j + 1)th digits of the result
+
+         1368
+         912
+        456 
+     + --------
+        56088
+    
+    the result of num1[i]  * num2[j] affects at most the (i + j)th digit of the result, and (i + j + 1)th.
+*/
+
+
+class Solution {
+    public String multiply(String num1, String num2) {
         if (num1.equals("0") || num2.equals("0")) {
             return "0";
         } 
@@ -55,25 +91,3 @@ class Solution {
 
 
 
-class Solution2 {
-    public String multiply(String num1, String num2) {
-        int m = num1.length(), n = num2.length();
-        int[] pos = new int[m + n]; // num1 * num2 must have m + n digits
-   
-        for(int i = m - 1; i >= 0; i--) {
-            for(int j = n - 1; j >= 0; j--) {
-                int mul = (num1.charAt(i) - '0') * (num2.charAt(j) - '0'); 
-                int p1 = i + j, p2 = i + j + 1;
-                int sum = mul + pos[p2];
-
-                pos[p1] += sum / 10;
-                pos[p2] = (sum) % 10;
-            }
-        }  
-    
-        StringBuilder sb = new StringBuilder();
-        for(int p : pos) if(!(sb.length() == 0 && p == 0)) sb.append(p);
-        return sb.length() == 0 ? "0" : sb.toString();
-    }
-}
-// `num1[i] * num2[j]` will be placed at indices `[i + j`, `i + j + 1]`
